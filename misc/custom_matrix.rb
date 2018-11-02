1cd7141ee49d81249a34f74b0185debcce7bf91c
Francesco Meggetto <fra_megg@hotmail.com>
2018-11-02 11:46:46 +0000

    Remove cycles function

--- misc/custom_matrix.rb
+++ misc/custom_matrix.rb
@@ -1,10 +1,12 @@
 require 'matrix'
 
 class CustomMatrix
-  def initialize(matrix)
+  def initialize(matrix, cycles: false)
     @original = matrix
     @row_count = @original.row_count
     @column_count = @original.column_count
+    @multiply_matrix = @original
+    @cycles = cycles
   end
 
   def print(matrix)
@@ -14,50 +16,53 @@ class CustomMatrix
   def power_set(power)
     return Matrix.build(@row_count, @column_count) { [] } if power == 1
 
-    (2..power).each { |n| @multiply_matrix = multiplication(n) }
+    (2..power).each { @multiply_matrix = multiplication }
     @multiply_matrix
   end
 
   private
 
   # https://github.com/ruby/matrix/blob/master/lib/matrix.rb#L893
-  def multiplication(power_number)
+  def multiplication
     rows = Array.new(@row_count) do |i|
       Array.new(@column_count) do |j|
-        (0...@column_count).inject([]) do |vij, k|
-          method_name = power_number == 2 ? 'second' : 'other'
-          send("compute_#{method_name}_power", i, j, k, vij)
+        item = (0...@column_count).inject([]) do |vij, k|
+          compute_power(i, j, k, vij)
         end
+        item = remove_potential_cycles(item, i, j) unless @cycles
+        item
       end
     end
 
     Matrix.rows(rows)
   end
 
-  def compute_second_power(index_i, index_j, index_k, vij)
-    if @original[index_k, index_j].zero? || @original[index_i, index_k].zero?
-      vij.concat([])
-    else
-      vij.concat([index_k + 1])
-    end
+  def compute_power(index_i, index_j, index_k, vij)
+    multiply_matrix_values = @multiply_matrix[index_i, index_k]
+    val = if @original[index_k, index_j] == [] || multiply_matrix_values.empty?
+            []
+          elsif multiply_matrix_values == [true]
+            [[index_k + 1]]
+          else
+            multiply_matrix_values.map { |i| i + [index_k + 1] }
+          end
+    vij.concat(val)
   end
 
-  def compute_other_power(index_i, index_j, index_k, vij)
-    multiply_matrix_values = @multiply_matrix[index_i, index_k]
-    if @original[index_k, index_j].zero? || multiply_matrix_values.empty?
-      vij.concat([])
-    else
-      vij.concat(multiply_matrix_values.map { |i| [i].flatten + [index_k + 1] })
+  def remove_potential_cycles(item, index_i, index_j)
+    item.each do |value|
+      item = [] if value.include?(index_i + 1) || value.include?(index_j + 1)
     end
+    item
   end
 end
 
 # Class initialization and calling
 input_matrix = Matrix[
-  [0, 1, 0, 0],
-  [1, 0, 1, 1],
-  [0, 1, 0, 1],
-  [0, 1, 1, 0]
+  [[], [true], [], []],
+  [[true], [], [true], [true]],
+  [[], [true], [], [true]],
+  [[], [true], [true], []]
 ]
 
 my_custom_matrix = CustomMatrix.new(input_matrix)
