4760afd05ca5fb8c5e9d19ed3d475bb0722637cf
Francesco Meggetto <fra_megg@hotmail.com>
2018-12-03 15:01:20 +0000

    Greedy implementation for k resources

--- misc/greedy_algorithm.rb
+++ misc/greedy_algorithm.rb
@@ -9,97 +9,84 @@ class GreedyAlgorithm
     @path_length = path_length
     @number_resources = number_resources
     @cycles = cycles
-
     @my_custom_matrix = CustomMatrix.new(matrix, cycles: @cycles)
     @resulting_matrix = @my_custom_matrix.power_set(@path_length)
-
-    @dispatched_nodes = []
+    @solution = []
   end
 
   def apply
-    k = @number_resources
-    1.upto(k) do |i|
-      list_paths = @resulting_matrix.to_a
-      remove_paths_with_i_resources!(list_paths, i)
-      p 'lists'
-      p list_paths
-      sub_solution_one_resource(list_paths)
-    end
-  end
+    list_paths = retrieve_paths
+    # add first K most frequent nodes in @solution
+    s_nodes = find_most_frequent_nodes(list_paths, @number_resources)
+    @solution.concat(s_nodes)
 
-  def remove_paths_with_i_resources!(list_paths, i_resources)
-    # remove empty paths
-    list_paths.each { |paths| paths.reject!(&:empty?) }
-    return if @dispatched_nodes.empty?
-
-    list_paths.each do |paths|
-      paths.each do |path|
-        path.reject! { |x| (x & @dispatched_nodes).count == i_resources }
-      end
-      paths.reject!(&:empty?)
+    until list_paths.empty?
+      remove_paths!(list_paths)
+      node = find_most_frequent_nodes(list_paths, 1)
+      @solution.concat(node) unless node.nil?
     end
   end
 
   def print_result
     puts 'Original Matrix'
     puts @my_custom_matrix.print_original_matrix
-    puts "Dispatch nodes in: #{@dispatched_nodes}"
+    puts "Dispatch nodes in: #{@solution}"
   end
 
   private
 
-  def sub_solution_one_resource(matrix)
-    array_matrix = matrix
-    remove_paths_with_present_node!(array_matrix)
-    return if array_matrix.all?(&:empty?)
-
-    @dispatched_nodes << find_most_frequent_value(array_matrix)
-    sub_solution_one_resource(matrix)
+  def retrieve_paths
+    list_paths = @resulting_matrix.to_a
+    # remove empty paths
+    list_paths.each { |paths| paths.reject!(&:empty?) }
   end
 
-  def remove_paths_with_present_node!(list_paths)
-    # TODO : code duplication. Use blocks and "yield"
-    list_paths.each do |paths|
+  def remove_paths!(list_paths)
+    # remove paths in L containing K vertices in @solution as well as removing
+    # empty paths. This will speed up the process in the long run
+    new_paths = list_paths.each do |paths|
       paths.each do |path|
-        path.select! { |x| (x & @dispatched_nodes).count.zero? }
+        path.reject! { |x| (x & @solution).count == @number_resources }
       end
       paths.reject!(&:empty?)
     end
+    new_paths.reject!(&:empty?)
   end
 
-  def find_most_frequent_value(array_matrix)
-    freq = array_matrix.flatten.each_with_object(Hash.new(0)) do |v, h|
-      h[v] += 1
-      h
+  def find_most_frequent_nodes(list_paths, n_resources)
+    frequencies = frequency_hash(list_paths)
+    top_vals = frequencies.values.max_by(n_resources) { |i| i }
+    most_frequent_nodes(frequencies, top_vals)
+  end
+
+  def frequency_hash(list_paths)
+    list_paths.flatten.each_with_object(Hash.new(0)) do |val, hash|
+      hash[val] += 1
+      hash
     end
-    max = freq.values.max
-    top_finds = freq.select { |_, f| f == max }
-    top_finds.keys.sample
+  end
+
+  def most_frequent_nodes(frequencies, top_values)
+    most_frequent_nodes = []
+    top_values.each do |value|
+      top_finds = frequencies.select { |_, frequency| frequency == value }
+      most_frequent_nodes << top_finds.keys.first
+    end
+    most_frequent_nodes
   end
 end
 
 # Class initialization and calling
-=begin
 input_matrix = Matrix[
   [0, 1, 0, 0],
   [1, 0, 1, 1],
   [0, 1, 0, 1],
   [0, 1, 1, 0]
 ]
-=end
 
-input_matrix = Matrix[
-  [0, 1, 1, 0, 1, 0, 0],
-  [1, 0, 0, 1, 1, 1, 1],
-  [1, 0, 0, 1, 1, 0, 1],
-  [0, 1, 1, 0, 0, 0, 0],
-  [1, 1, 1, 0, 0, 0, 1],
-  [0, 1, 0, 0, 0, 0, 1],
-  [0, 1, 1, 0, 1, 1, 0]
-]
 algorithm = GreedyAlgorithm.new(input_matrix,
-                                path_length: 5,
-                                number_resources: 2,
-                                cycles: true)
+                                path_length: 3,
+                                number_resources: 1,
+                                cycles: false)
 algorithm.apply
 algorithm.print_result
