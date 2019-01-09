483f572d285352a95e1f39e9a4e9ad1c3477a53e
Francesco Meggetto <fra_megg@hotmail.com>
2019-01-09 13:45:24 +0000

    Starting API project

---
+++ gems.rb
@@ -0,0 +1,26 @@
+source 'https://rubygems.org'
+
+gem 'mysql2', '>= 0.4.4', '< 0.6.0'
+gem 'puma', '~> 3.11'
+gem 'rails', '~> 5.2.2'
+gem 'sidekiq'
+
+group :development, :test do
+  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'rubocop', '~> 0.62.0'
+  gem 'simplecov'
+end
+
+group :development do
+  gem 'better_errors'
+  gem 'binding_of_caller'
+  gem 'flamegraph'
+  gem 'listen', '>= 3.0.5', '< 3.2'
+  gem 'rack-mini-profiler', '~> 0.10'
+  gem 'stackprof'
+  gem 'web-console', '>= 3.3.0'
+end
+
+
+# Windows does not include zoneinfo files, so bundle the tzinfo-data gem
+gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
