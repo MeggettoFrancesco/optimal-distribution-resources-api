49aa1e2b17ac86a224db3dde319e01073b47f48e
Francesco Meggetto <fra_megg@hotmail.com>
2019-01-09 13:50:55 +0000

    Add rubocop

--- gems.rb
+++ gems.rb
@@ -6,7 +6,7 @@ gem 'rails', '~> 5.2.2'
 gem 'sidekiq'
 
 group :development, :test do
-  gem 'byebug', platforms: [:mri, :mingw, :x64_mingw]
+  gem 'byebug', platforms: %i[mri mingw x64_mingw]
   gem 'rubocop', '~> 0.62.0'
   gem 'simplecov'
 end
@@ -21,6 +21,5 @@ group :development do
   gem 'web-console', '>= 3.3.0'
 end
 
-
 # Windows does not include zoneinfo files, so bundle the tzinfo-data gem
-gem 'tzinfo-data', platforms: [:mingw, :mswin, :x64_mingw, :jruby]
+gem 'tzinfo-data', platforms: %i[mingw mswin x64_mingw jruby]
