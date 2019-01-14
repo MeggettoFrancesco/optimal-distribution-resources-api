593fd861682e70599d69450329f2171ce69068d3
Francesco Meggetto <fra_megg@hotmail.com>
2019-01-14 16:24:10 +0000

    Update admin controllers

--- app/admin/greedy_algorithms.rb
+++ app/admin/greedy_algorithms.rb
@@ -1,3 +1,39 @@
 ActiveAdmin.register GreedyAlgorithm do
   menu parent: 'API Requests'
+  actions :all, except: :edit
+
+  filter :path_length
+  filter :number_resources
+  filter :cycles
+  filter :created_at
+  filter :updated_at
+
+  index do |r|
+    selectable_column
+    id_column
+    column :input_matrix
+    column :path_length
+    column :number_resources
+    column :cycles
+    column :solution
+    column :request
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  show do |r|
+    attributes_table do
+      row :id
+      row :input_matrix
+      row :path_length
+      row :number_resources
+      row :cycles
+      row :solution
+      row :request
+      row :created_at
+      row :updated_at
+      active_admin_comments
+    end
+  end
 end
--- app/admin/requests.rb
+++ app/admin/requests.rb
@@ -1,3 +1,40 @@
 ActiveAdmin.register Request do
   menu parent: 'API Requests'
+
+  permit_params :request_type, :ip_address
+
+  filter :request_type, as: :select, collection: Request.request_type.values
+  filter :ip_address
+  filter :created_at
+  filter :updated_at
+
+  index do |r|
+    selectable_column
+    id_column
+    column :request_type
+    column :ip_address
+    column :created_at
+    column :updated_at
+    actions
+  end
+
+  form do |f|
+    f.inputs do
+      f.input :ip_address
+    end
+    f.actions
+  end
+
+  show do |r|
+    attributes_table do
+      row :id
+      row :request_type
+      Request.request_type.values.each do |t|
+        row t if r.send(t).present?
+      end
+      row :created_at
+      row :updated_at
+      active_admin_comments
+    end
+  end
 end
