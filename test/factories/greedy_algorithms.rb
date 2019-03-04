05f428ae80b0af916645fb68578fc8aa4f64e184
Francesco Meggetto <fra_megg@hotmail.com>
2019-03-04 16:12:21 +0100

    Sidekiq worker test

--- test/factories/greedy_algorithms.rb
+++ test/factories/greedy_algorithms.rb
@@ -1,8 +1,8 @@
 FactoryBot.define do
   factory :greedy_algorithm do
     input_matrix { create_matrix }
-    path_length { rand(3..10) }
-    number_resources { path_length - 1 }
+    path_length { [3, 4].sample }
+    number_resources { [1, 2].sample }
     cycles { Faker::Boolean.boolean }
   end
 end
@@ -10,7 +10,7 @@ end
 private
 
 def create_matrix
-  size = rand(4..25)
+  size = rand(4..7)
 
   Array.new(size) do |row|
     Array.new(size) do |col|
--- test/workers/execute_algorithm_worker_test.rb
+++ test/workers/execute_algorithm_worker_test.rb
@@ -1,3 +1,19 @@
 require 'test_helper'
+require 'sidekiq/testing'
+
 class ExecuteAlgorithmWorkerTest < MiniTest::Test
+  include FactoryBot::Syntax::Methods
+
+  def setup
+    Sidekiq::Worker.clear_all
+    @my_greedy_algorithm = create(:request).greedy_algorithm
+    @my_greedy_algorithm.compute_solution
+  end
+
+  def test_solution_is_updated
+    ExecuteAlgorithmWorker.drain
+
+    assert_equal 0, ExecuteAlgorithmWorker.jobs.size
+    assert @my_greedy_algorithm.reload.solution
+  end
 end
